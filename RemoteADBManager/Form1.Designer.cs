6df93854a7cd6f13f110db81e563ae1fa44e03c5
Robert Brown <brownrc@live.com>
2017-05-17 15:38:39 -0500

    Initial Commit

---
+++ RemoteADBManager/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace RemoteADBManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.ipTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.disconnectButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.label8 = new System.Windows.Forms.Label();
+            this.findWirelessAddressButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.verboseTextBox = new System.Windows.Forms.TextBox();
+            this.helpButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.helpButton);
+            this.splitContainer1.Panel1.Controls.Add(this.ipTextBox);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.tabControl1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.verboseTextBox);
+            this.splitContainer1.Size = new System.Drawing.Size(705, 466);
+            this.splitContainer1.SplitterDistance = 235;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // ipTextBox
+            // 
+            this.ipTextBox.Location = new System.Drawing.Point(76, 10);
+            this.ipTextBox.Name = "ipTextBox";
+            this.ipTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ipTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Current IP";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(12, 36);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(681, 196);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.connectButton);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.label7);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(673, 170);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Connect Device";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // connectButton
+            // 
+            this.connectButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.connectButton.Location = new System.Drawing.Point(592, 141);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(75, 23);
+            this.connectButton.TabIndex = 5;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 59);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(130, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Press the Connect button.";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 31);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(302, 13);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Make sure that the IP address for the device is entered above.";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.disconnectButton);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(673, 170);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Disconnect Device";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // disconnectButton
+            // 
+            this.disconnectButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.disconnectButton.Location = new System.Drawing.Point(592, 141);
+            this.disconnectButton.Name = "disconnectButton";
+            this.disconnectButton.Size = new System.Drawing.Size(75, 23);
+            this.disconnectButton.TabIndex = 3;
+            this.disconnectButton.Text = "Disconnect";
+            this.disconnectButton.UseVisualStyleBackColor = true;
+            this.disconnectButton.Click += new System.EventHandler(this.disconnectButton_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 59);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(168, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Then click the Disconnect button.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(302, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Make sure that the IP address for the device is entered above.";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.label8);
+            this.tabPage3.Controls.Add(this.findWirelessAddressButton);
+            this.tabPage3.Controls.Add(this.label3);
+            this.tabPage3.Controls.Add(this.label2);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(673, 170);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Find Device Wi-Fi";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 31);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(322, 13);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Connect the device to the same wireless network as this computer.";
+            // 
+            // findWirelessAddressButton
+            // 
+            this.findWirelessAddressButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.findWirelessAddressButton.Location = new System.Drawing.Point(592, 141);
+            this.findWirelessAddressButton.Name = "findWirelessAddressButton";
+            this.findWirelessAddressButton.Size = new System.Drawing.Size(75, 23);
+            this.findWirelessAddressButton.TabIndex = 2;
+            this.findWirelessAddressButton.Text = "Execute";
+            this.findWirelessAddressButton.UseVisualStyleBackColor = true;
+            this.findWirelessAddressButton.Click += new System.EventHandler(this.findWirelessAddressButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Then press the Execute button.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(198, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Please connect the device through USB";
+            // 
+            // verboseTextBox
+            // 
+            this.verboseTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.verboseTextBox.Location = new System.Drawing.Point(0, 0);
+            this.verboseTextBox.Multiline = true;
+            this.verboseTextBox.Name = "verboseTextBox";
+            this.verboseTextBox.ReadOnly = true;
+            this.verboseTextBox.Size = new System.Drawing.Size(705, 227);
+            this.verboseTextBox.TabIndex = 0;
+            // 
+            // helpButton
+            // 
+            this.helpButton.Location = new System.Drawing.Point(614, 8);
+            this.helpButton.Name = "helpButton";
+            this.helpButton.Size = new System.Drawing.Size(75, 23);
+            this.helpButton.TabIndex = 3;
+            this.helpButton.Text = "Help";
+            this.helpButton.UseVisualStyleBackColor = true;
+            this.helpButton.Click += new System.EventHandler(this.helpButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 466);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximumSize = new System.Drawing.Size(721, 504);
+            this.MinimumSize = new System.Drawing.Size(721, 504);
+            this.Name = "Form1";
+            this.Text = "Remote Connection ADB Manager";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox verboseTextBox;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox ipTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button disconnectButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button findWirelessAddressButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button helpButton;
+    }
+}
+
---
+++ RemoteADBManager/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace RemoteADBManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            if (ipTextBox.Text.Equals("") || ipTextBox.Text == null)
+            {
+                verboseTextBox.Text += "Please enter an IP address or find one." + Environment.NewLine;
+            }
+            else
+            {
+                executeShellCommand("adb tcpip 5555");
+                executeShellCommand("adb connect " + ipTextBox.Text);
+            }
+        }
+
+        private void disconnectButton_Click(object sender, EventArgs e)
+        {
+            executeShellCommand("adb disconnect " + ipTextBox.Text);
+        }
+
+        private void findWirelessAddressButton_Click(object sender, EventArgs e)
+        {
+            executeShellCommand("adb shell ip -f inet addr show wlan0");
+
+            if (verboseTextBox.Text.Contains("wlan0") && verboseTextBox.Text.Contains("inet"))
+            {
+                int start = verboseTextBox.Text.IndexOf("inet ");
+                int end = verboseTextBox.Text.IndexOf("/");
+
+                string ip = verboseTextBox.Text.Substring(start, (end - start)).Replace("inet ", "");
+                ipTextBox.Text = ip;
+            }
+        }
+
+        // Uses: http://stackoverflow.com/questions/20992208/execute-shell-with-custom-command
+        //       http://stackoverflow.com/questions/4291912/process-start-how-to-get-the-output
+        private void executeShellCommand(string command)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo("cmd", "/c" + command);
+            startInfo.UseShellExecute = false;
+            startInfo.CreateNoWindow = true;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+            Process process = new Process();
+            process.StartInfo = startInfo;
+            process.Start();
+
+            while (!process.StandardOutput.EndOfStream)
+            {
+                string line = process.StandardOutput.ReadLine() + Environment.NewLine;
+
+                verboseTextBox.Text += line;
+            }
+
+            while (!process.StandardError.EndOfStream)
+            {
+                string line = process.StandardError.ReadLine() + Environment.NewLine;
+
+                verboseTextBox.Text += line;
+            }
+
+            process.WaitForExit();
+        }
+
+        private void helpButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("1.) Make sure you have the Android SDK installed." + Environment.NewLine +
+                "2.) Make sure you have your ANDROID_HOME environment variable set in order to give access to adb." + Environment.NewLine +
+                "3.) Make sure you have a good USB cable for the commands that need it.");
+        }
+    }
+}
